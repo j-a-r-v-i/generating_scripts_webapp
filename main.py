260090178deea7ef391c54fe99e90fe230eae2d8
Archit Bansal <archit.bansal94069@gmail.com>
2020-09-30 21:36:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,13 +3,10 @@
 from flask import Flask,render_template,url_for,request
 import torch
 import helper
-import problem_unittests as tests
 import numpy as np
 import torch.nn as nn
-from model import RNN
-import torch
 
-    
+
 
 app=Flask(__name__)
 
@@ -21,23 +18,118 @@ def home():
 
 def predict():
     
+    
 
-    sequence_length =10   # of words in a sequence
-    # Batch Size
-    batch_size = 130
 
+    # Check for a GPU
     train_on_gpu = torch.cuda.is_available()
     if not train_on_gpu:
         print('No GPU found. Please use a GPU to train your neural network.')
+    _, vocab_to_int, int_to_vocab, token_dict = helper.load_preprocess()
     
-
+    
+    class RNN(nn.Module):
         
-
-
-
-
-
-   
+        def __init__(self, vocab_size, output_size, embedding_dim, hidden_dim, n_layers, dropout=0.5):
+            """
+            Initialize the PyTorch RNN Module
+            :param vocab_size: The number of input dimensions of the neural network (the size of the vocabulary)
+            :param output_size: The number of output dimensions of the neural network
+            :param embedding_dim: The size of embeddings, should you choose to use them        
+            :param hidden_dim: The size of the hidden layer outputs
+            :param dropout: dropout to add in between LSTM/GRU layers
+            """
+            super(RNN, self).__init__()
+            # TODO: Implement function
+            
+            # set class variables
+            self.output_size = output_size
+            self.n_layers = n_layers
+            self.hidden_dim = hidden_dim
+            
+            # define model layers
+            
+            # embedding and LSTM layers
+            self.embed = nn.Embedding(vocab_size, embedding_dim)
+            self.lstm = nn.LSTM(embedding_dim, hidden_dim, n_layers, dropout=dropout, batch_first=True)
+                    
+            # linear layer
+            self.fc = nn.Linear(hidden_dim, output_size)
+        
+        
+        def forward(self, nn_input, hidden):
+            """
+            Forward propagation of the neural network
+            :param nn_input: The input to the neural network
+            :param hidden: The hidden state        
+            :return: Two Tensors, the output of the neural network and the latest hidden state
+            """
+            # TODO: Implement function   
+            batch_size = nn_input.size(0)
+    
+            # embeddings and lstm_out
+            embeds = self.embed(nn_input)
+            lstm_out, hidden = self.lstm(embeds, hidden)
+            # stack up lstm outputs
+            lstm_out = lstm_out.contiguous().view(-1, self.hidden_dim)
+            
+            # dropout and fully-connected layer
+            output = self.fc(lstm_out)
+            
+            # reshape to be batch_size first
+            output = output.view(batch_size, -1, self.output_size)
+            out = output[:, -1] # get last batch of labels       
+            # return one batch of output word scores and the hidden state
+            return out, hidden
+           
+        
+        
+        
+        def init_hidden(self, batch_size):
+            '''
+            Initialize the hidden state of an LSTM/GRU
+            :param batch_size: The batch_size of the hidden state
+            :return: hidden state of dims (n_layers, batch_size, hidden_dim)
+            '''
+            # Implement function
+            
+            # initialize hidden state with zero weights, and move to GPU if available
+          # initialize hidden state with zero weights, and move to GPU if available
+            weight = next(self.parameters()).data
+            
+            if (train_on_gpu):
+                hidden = (weight.new(self.n_layers, batch_size, self.hidden_dim).zero_().cuda(),
+                          weight.new(self.n_layers, batch_size, self.hidden_dim).zero_().cuda())
+            else:
+                hidden = (weight.new(self.n_layers, batch_size, self.hidden_dim).zero_(),
+                          weight.new(self.n_layers, batch_size, self.hidden_dim).zero_())
+            
+            return hidden
+    
+    
+    # Model parameters
+    # Vocab size
+    vocab_size = len(vocab_to_int)
+    # Output size
+    output_size = vocab_size
+    # Embedding Dimension
+    embedding_dim = 256
+    # Hidden Dimension
+    hidden_dim = 256
+    # Number of RNN Layers
+    n_layers = 2
+    sequence_length =8   
+    
+    trained_rnn = RNN(vocab_size, output_size, embedding_dim, hidden_dim, n_layers, dropout=0.5)
+    
+    
+    
+    
+    trained_rnn.load_state_dict(torch.load('trained_rnn1.pt',map_location=torch.device('cpu')))
+    
+    """
+    DON'T MODIFY ANYTHING IN THIS CELL THAT IS BELOW THIS LINE
+    """
     import torch.nn.functional as F
     
     def generate(rnn, prime_id, int_to_vocab, token_dict, pad_value, predict_len=100):
@@ -103,13 +195,7 @@ def predict():
         
         # return all the sentences
         return gen_sentences
-
-    _, vocab_to_int, int_to_vocab, token_dict = helper.load_preprocess()
-    trained_rnn = helper.load_model('trained_rnn')
-
-        
     
-        
     if(request.method == 'POST'):
         message = request.form['message']
         gen_length = 400 # modify the length to your preference
@@ -118,6 +204,7 @@ def predict():
         generated_script = generate(trained_rnn, vocab_to_int[prime_word + ':'], int_to_vocab, token_dict, vocab_to_int[pad_word], gen_length)
 		
         return render_template('result.html',prediction = generated_script)
+
     
 if __name__ == '__main__':
 	app.run(debug=True)
